885c83412edf56652a643233a39829fcc9f1d6fb
John Mark Redding <johomurk@gmail.com>
2019-01-29 12:22:44 -0500

    Done.

--- src/components/EyesOnMe.js
+++ src/components/EyesOnMe.js
@@ -1,1 +1,18 @@
-// Code EyesOnMe Component Here
+import React from 'react';
+
+class EyesOnMe extends React.Component {
+  constructor() {
+    super();
+  }
+  focusHandler(e) {
+    console.log("Good!");
+  }
+  blurHandler(e) {
+    console.log("Hey! Eyes on me!");
+  }
+  render() {
+    return <button onFocus={this.focusHandler} onBlur={this.blurHandler}>Enter</button>;
+  }
+}
+
+export default EyesOnMe;
--- src/index.js
+++ src/index.js
@@ -1,13 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-import Keypad from './components/Keypad';
-import EyesOnMe from './components/EyesOnMe';
+import Keypad from './components/Keypad.js';
+import EyesOnMe from './components/EyesOnMe.js';
 
 ReactDOM.render(
-  <div>
+  (<div>
     <Keypad />
     <EyesOnMe />
-  </div>,
-  document.getElementById('global')
-);
+  </div>), document.getElementById('global'));
